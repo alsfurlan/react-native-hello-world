20e83a08ffe358f5637975854c926b027af7890b
Anderson Furlan <anderson.furlan@thomsonreuters.com>
2022-12-03 10:01:45 -0300

    Hello world

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onPressHandler = () => {
+    console.log("click");
+    const n = name && name.trim();
+    if (n) {
+      setMessage(`Hello world, ${n}!`);
+    }
+  };
+
+  const onChangeTextHandler = (text) => {
+    console.log(text);
+    setName(text);
+    setMessage("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Text style={styles.title}>Hello World</Text>
+      <TextInput
+        style={styles.helloWorldInput}
+        placeholder="Digite o seu nome"
+        onChangeText={onChangeTextHandler}
+        value={name}
+      />
+      <Button
+        onPress={onPressHandler}
+        color="green"
+        title="Clique aqui para dizer oi"
+      />
+      {message && <Text style={styles.title}>{message}</Text>}
     </View>
   );
 }
@@ -13,8 +44,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontFamily: "monospace",
+    fontSize: 36,
+    color: "green",
+  },
+  helloWorldInput: {
+    fontSize: 24,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 16,
+    padding: 32,
+    margin: 16,
+    width: "80%",
+    backgroundColor: "green",
+    fontFamily: "monospace",
   },
 });
